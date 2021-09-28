0100dab9efeba2e1d878c408337fb160e5948df0
noidesantos@gmail.com <noidesantos@gmail.com>
2021-09-27 23:19:49 -0300

    First version

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Cobrinha_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb_snake = new System.Windows.Forms.Label();
+            this.pb_snake = new System.Windows.Forms.PictureBox();
+            this.bt_start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lb_points = new System.Windows.Forms.Label();
+            this.pn_points = new System.Windows.Forms.Panel();
+            this.lb_gameover = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snake)).BeginInit();
+            this.pn_points.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb_snake
+            // 
+            this.lb_snake.AutoSize = true;
+            this.lb_snake.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_snake.Location = new System.Drawing.Point(200, 21);
+            this.lb_snake.Name = "lb_snake";
+            this.lb_snake.Size = new System.Drawing.Size(96, 31);
+            this.lb_snake.TabIndex = 0;
+            this.lb_snake.Text = "Snake";
+            // 
+            // pb_snake
+            // 
+            this.pb_snake.Image = ((System.Drawing.Image)(resources.GetObject("pb_snake.Image")));
+            this.pb_snake.Location = new System.Drawing.Point(83, 69);
+            this.pb_snake.Name = "pb_snake";
+            this.pb_snake.Size = new System.Drawing.Size(329, 270);
+            this.pb_snake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_snake.TabIndex = 1;
+            this.pb_snake.TabStop = false;
+            // 
+            // bt_start
+            // 
+            this.bt_start.BackColor = System.Drawing.Color.Gold;
+            this.bt_start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_start.Location = new System.Drawing.Point(83, 345);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(329, 62);
+            this.bt_start.TabIndex = 2;
+            this.bt_start.Text = "START";
+            this.bt_start.UseVisualStyleBackColor = false;
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // lb_points
+            // 
+            this.lb_points.AutoSize = true;
+            this.lb_points.BackColor = System.Drawing.Color.Black;
+            this.lb_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_points.ForeColor = System.Drawing.Color.White;
+            this.lb_points.Location = new System.Drawing.Point(3, 1);
+            this.lb_points.Name = "lb_points";
+            this.lb_points.Size = new System.Drawing.Size(67, 15);
+            this.lb_points.TabIndex = 3;
+            this.lb_points.Text = "Pontos: 0";
+            this.lb_points.Visible = false;
+            // 
+            // pn_points
+            // 
+            this.pn_points.BackColor = System.Drawing.Color.Black;
+            this.pn_points.Controls.Add(this.lb_points);
+            this.pn_points.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pn_points.Location = new System.Drawing.Point(0, 0);
+            this.pn_points.Name = "pn_points";
+            this.pn_points.Size = new System.Drawing.Size(484, 20);
+            this.pn_points.TabIndex = 4;
+            this.pn_points.Visible = false;
+            // 
+            // lb_gameover
+            // 
+            this.lb_gameover.AutoSize = true;
+            this.lb_gameover.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_gameover.ForeColor = System.Drawing.Color.Firebrick;
+            this.lb_gameover.Location = new System.Drawing.Point(157, 201);
+            this.lb_gameover.Name = "lb_gameover";
+            this.lb_gameover.Size = new System.Drawing.Size(171, 31);
+            this.lb_gameover.TabIndex = 5;
+            this.lb_gameover.Text = "Game Over!";
+            this.lb_gameover.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb_gameover.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.lb_gameover);
+            this.Controls.Add(this.pn_points);
+            this.Controls.Add(this.bt_start);
+            this.Controls.Add(this.pb_snake);
+            this.Controls.Add(this.lb_snake);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snake)).EndInit();
+            this.pn_points.ResumeLayout(false);
+            this.pn_points.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_snake;
+        private System.Windows.Forms.PictureBox pb_snake;
+        private System.Windows.Forms.Button bt_start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lb_points;
+        private System.Windows.Forms.Panel pn_points;
+        private System.Windows.Forms.Label lb_gameover;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cobrinha_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Label snake = new Label();
+        int direction = 0;
+        Random position = new Random();
+        List<Control> snakes = new List<Control>() { };
+        List<Point> points = new List<Point>() { };
+        int pts = 0;
+        int time = 200;
+        bool tickstate = false;
+
+        private void bt_start_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            
+            lb_snake.Visible = false;
+            pb_snake.Visible = false;
+            bt_start.Visible = false;
+
+            snake.Text = "▉";
+            Font f = new Font("Arial", 8, FontStyle.Bold);
+            snake.Font = f;
+            snake.ForeColor = Color.Black;
+            snake.BackColor = Color.Black;
+            snake.Visible = true;
+            this.Controls.Add(snake);
+
+            Point p = new Point(this.Width / 2, Convert.ToInt32(this.Height * 0.4));
+            snake.Location = p;
+
+            Size s = new Size(10, 10);
+            snake.Size = s;
+
+            points.Add(snake.Location);
+
+            bt_start.Dispose();
+
+            new_snake_piece("piece_1");
+
+            snakes.Add(snake);
+
+            pn_points.Visible = true;
+            lb_points.Visible = true;
+
+            timer1.Enabled = true;
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (direction != 2)
+                {
+                    direction = 1;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (direction != 1)
+                {
+                    direction = 2;
+                }
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (direction != 4)
+                {
+                    direction = 3;
+                }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (direction != 3)
+                {
+                    direction = 4;
+                }
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            for (int i = points.Count - 1; i >= 1; i--)
+            {
+                points[i] = points[i - 1];
+            }
+
+            if (snakes.Count > 1)
+            {
+                for (int i = 1; i < snakes.Count; i++)
+                {
+                    snakes[i].Location = points[i];
+                }
+            }
+
+            if (direction == 1)
+            {
+                snake.Top -= 10;
+            }
+            else if (direction == 2)
+            {
+                snake.Top += 10;
+            }
+            else if (direction == 3)
+            {
+                snake.Left -= 10;
+            }
+            else if (direction == 4)
+            {
+                snake.Left += 10;
+            }
+
+            if (snake.Left < 0)
+            {
+                snake.Left = this.Width - 30;
+            }
+
+            if (snake.Left > this.Width - 30)
+            {
+                snake.Left = 0;
+            }
+
+            if (snake.Top < 20)
+            {
+                snake.Top = this.Height - 50;
+            }
+
+            if (snake.Top > this.Height - 50)
+            {
+                snake.Top = 20;
+            }
+
+            points[0] = snake.Location;
+
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name.Contains("piece"))
+                {
+                    if (snake.Location == c.Location)
+                    {
+                        if (!snakes.Contains(c))
+                        {
+                            snakes.Add(c);
+                            new_snake_piece("piece_" + snakes.Count.ToString());
+                            pts += 1;
+                            lb_points.Text = "Pontos: " + pts.ToString();
+                            if (time > 20)
+                            {
+                                time -= 3;
+                                timer1.Interval = time;
+                            }
+                        }
+                        else
+                        {
+                            timer1.Enabled = false;
+                            System.Threading.Thread.Sleep(1000);
+                            lb_gameover.Text = "Game Over!" + Environment.NewLine + "Pontos: " + pts.ToString();
+                            pn_points.Visible = false;
+                            foreach (Control ctr in this.Controls)
+                            {
+                                if (ctr.Name.Contains("snake") || ctr.Name.Contains("piece"))
+                                {
+                                    c.Visible = false;
+                                }
+                            }
+                            lb_gameover.Visible = true;
+                            this.Update();
+                            System.Threading.Thread.Sleep(5000);
+                            Application.Restart();
+                        }
+                    }
+                }
+            }
+
+            tickstate = !tickstate;
+        }
+
+        private void new_snake_piece(string name)
+        {
+            Label sn = new Label();
+            sn.Text = "▉";
+            Font f = new Font("Arial", 8, FontStyle.Bold);
+            sn.Font = f;
+            sn.ForeColor = Color.Black;
+            sn.BackColor = Color.Black;
+            sn.Visible = true;
+            Point p = new Point(RoundUp(position.Next(10, 470)), RoundUp(position.Next(10, 440)));
+            bool contain = true;
+            while (contain)
+            {
+                bool equal = false;
+                for (int i=0; i<points.Count; i++)
+                {
+                    if (points[i] == p)
+                    {
+                        equal = true;
+                    }
+                }
+
+                if (equal)
+                {
+                    p = new Point(RoundUp(position.Next(20, 470)), RoundUp(position.Next(20, 430)));
+                }
+                else
+                {
+                    contain = false;
+                }
+            }
+
+            sn.Location = p;
+            Size s = new Size(10, 10);
+            sn.Size = s;
+            this.Controls.Add(sn);
+            points.Add(sn.Location);
+            sn.Name = name;
+        }
+
+        private int RoundUp(int toRound)
+        {
+            if (toRound % 10 == 0) return toRound;
+            return (10 - toRound % 10) + toRound;
+        }
+    }
+}
